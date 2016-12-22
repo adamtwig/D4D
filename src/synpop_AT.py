e88ab39b332c9461df4f567b02e5f75502e40289
Adam Terwilliger <terwilla@mail.gvsu.edu>
2016-12-22 02:31:58 -0500

    normalizing markov chain

--- src/synpop_AT.py
+++ src/synpop_AT.py
@@ -43,25 +43,26 @@ def main():
     timeDist = np.zeros(24*6)
 
     # no user makes more than 1000 trips in one day
+    # reshape once find max num trips
     userLengths = np.zeros(1000)
-    
 
     user_id = 1
     num_trips = 1
+    maxNum_trips = 1
     #for x in range(100):	 
     for x in range(len(data_array_2d)):
 
         user_curr = int(data_array_2d[x][0])
-        ant_prev = int(data_array_2d[x][1])
-        ant_curr = int(data_array_2d[x][2])
-        time_prev = get_date(data_array_2d[x][3])
-        time_curr = get_date(data_array_2d[x][4])
+        ant_curr = int(data_array_2d[x][1])
+        ant_next = int(data_array_2d[x][2])
+        time_curr = get_date(data_array_2d[x][3])
+        time_next = get_date(data_array_2d[x][4])
 
         # step 1. generate markov chain counts 
-        markovChainCounts[ant_prev,ant_curr] += 1
+        markovChainCounts[ant_curr,ant_next] += 1
     
         # step 2. acculumate time of trips
-        time_index = int(time_curr.hour*6.0+time_curr.minute/10.0)
+        time_index = int(time_next.hour*6.0+time_next.minute/10.0)
         timeDist[time_index] +=1
 
         # step 3. acculumate trips per user
@@ -71,6 +72,8 @@ def main():
         else:
             userLengths[num_trips]+=1
             user_id = user_curr
+            if num_trips > maxNum_trips:
+                maxNum_trips = num_trips
             num_trips = 1   
  
         # overall debugging print statements
@@ -81,19 +84,29 @@ def main():
         if x % 10000 == 0:
             print "trip", x, "out of", len(data_array_2d)
 
+    # normalize markov chains so all outgoing trip prob = 1.0 (by row)
+    row_sums = markovChainCounts.sum(axis=1)
+    markovChain = np.nan_to_num(markovChainCounts / row_sums[:, np.newaxis])
+
     # markov chain 2d array debug
+    for row in range(len(markovChain)):
+        for col in range(len(markovChain[row])):
+            if markovChain[row,col] != 0:
+                print row, col, markovChain[row,col]
+
+
     #for row in range(len(markovChainCounts)):
     #    for col in range(len(markovChainCounts[row])):
     #        if markovChainCounts[row,col] != 0:
-    #            print row,col, markovChainCounts[row,col]
+    #            print row,col, markovChainCounts[row,col], sum(markovChainCounts[row]), markovChainCounts[row,col] / sum(markovChainCounts[row])
 
     # time dist array debug
     print timeDist
-    plt.plot(timeDist)
-    plt.show()
+    #plt.plot(timeDist)
+    #plt.show()
 
     # user length array debug
-    print userLengths
+    print userLengths[:maxNum_trips+1]
 
 if __name__ == "__main__":
     main()
